f698ba121d1c2bd8f159b72cba609f6911f3a130
dreamseeker.live <dreamseeker.live@gmail.com>
2023-06-02 15:12:08 +0530

    initial commit

---
+++ src/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Profile = () => {
+	  return (
+		      <div>
+		        <h1>Profile</h1>
+		        <p>Name: John Doe</p>
+		        <p>Age: 25</p>
+		        <p>Occupation: Software Developer</p>
+		      </div>
+		    );
+};
+
+export default Profile;
+
